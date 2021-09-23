4a00e2e588623bd9d42a9b3102d0fa29296abc7b
kimry <79510083+kimry@users.noreply.github.com>
2021-09-23 14:35:39 +0900

    Update using_account_info.php

--- server/User_management/using_account_info.php
+++ server/User_management/using_account_info.php
@@ -5,10 +5,10 @@ ini_set("display_errors", 1);
 */
 function findid($id)
 {
-    $dbHost="api.odyssea-ogc.com:13306";
-    $dbName="riyoung";
-    $dbUser="riyoung";
-    $dbPass="rlafldud";
+    $dbHost=""; // DBurl 입력
+    $dbName=""; // DBname 입력
+    $dbUser=""; // DBuser 입력
+    $dbPass=""; // DBpw 입력
     $pdo = new PDO("mysql:host={$dbHost};dbname={$dbName}",$dbUser,$dbPass);
     $statement =  $pdo -> query('select * from account_info where id="'.$id.'"');
     $row = $statement ->fetch();
